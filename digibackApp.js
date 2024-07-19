e1055f3bcbc76dfb8e70c50d1ccd501fdf7ee5e6
Alex Oliveira <alexoalves10@gmail.com>
2024-07-19 17:33:28 -0300

    configing database and Middleware

--- digibackApp.js
+++ digibackApp.js
@@ -17,3 +17,17 @@ digiback.use(express.json());
 
 //Configurando o Database
 const db = new sqlite3.Database(":memory:");
+const seedDatabase = require("./seed");
+
+digiback.use(express.json());
+// Middleware para autenticação
+const authenticateToken = (req, res, next) => {
+   const token = req.headers["authorization"];
+   if (!token) return res.sendStatus(401);
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+   });
+};
