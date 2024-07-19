ddf7ac71268a0d5a2224e5762650cf5276031c2a
Alex Oliveira <alexoalves10@gmail.com>
2024-07-19 17:22:11 -0300

    installing jsonwebtoken bcrypt and configing database

--- digibackApp.js
+++ digibackApp.js
@@ -5,9 +5,22 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const sqlite3 = require("sqlite3").verbose();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 //Instanciando o express
 const digiback = express();
 
 //Definindo a porta
 const PORT = process.env.PORT || 3000;
+digiback.use(express.json());
+
+//Configurando o Database
+const db = new sqlite3.Database(":memory:");
+
+//Criando a tabela de Digimons
+db.serialize(() => {
+   db.run(
+      "CREATE TABLE digimons (id INTEGER PRIMARY KEY, name TEXT, img TEXT, level TEXT)"
+   );
+});
